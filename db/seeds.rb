86f70e04de1280bbc496754daee7af62c3706757
joycejewell <joyceroop@gmail.com>
2022-07-31 18:49:37 -0600

    updated seed to include user with email and password encrypted

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+User.create(email: "username", password: BCrypt::Password.create('Your_Password'))
