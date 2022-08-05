fb89c14717e15e196d3ac2bf55edb18f33ca2f2c
joycejewell <joyceroop@gmail.com>
2022-08-04 20:11:56 -0600

    added routes for user, pin_drop, location and pin_drop_reaction

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,24 @@ Rails.application.routes.draw do
 
 
 #pin_drops
-  get ""
+  get "" => "pin_drop#ndex"
+  get "" => "pin_drop#show"
+  post "" => "pin_drop#create"
+  patch "" => "pin_drop#update"
+  delete "" => "pin_drop#destroy"
 
 #locations
-
+  get "" => "location#ndex"
+  get "" => "location#show"
+  post "" => "location#create"
+  patch "" => "location#update"
+  delete "" => "location#destroy"
 
 #pin_drop_reactions
-
+get "" => "pin_drop_reaction#ndex"
+get "" => "pin_drop_reaction#show"
+post "" => "pin_drop_reaction#create"
+patch "" => "pin_drop_reaction#update"
+delete "" => "pin_drop_reaction#destroy"
 
 end
