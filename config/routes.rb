4919c7977c383c5daa078c137a7f1c5c3f59fc33
joycejewell <joyceroop@gmail.com>
2022-08-11 19:57:00 -0600

    updated routes to clear errors in localhost:3000

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,36 @@
 Rails.application.routes.draw do
-#users
-  get "/user" => "user#index"
-  get "/user" => "user#show"
-  post "/user" => "user#create"
-  patch "/user" => "user#update"
-  delete "/user" => "user#destroy"
+get "/users/:id" => "users#show"
+get "pin_drops/:id" => "pin_drops#show"
+get "locations/:id" => "locations#show"
+get "pin_drop_reactions/:id" => "pin_drop_reactions#show"
 
+
+  #users
+  get "/users" => "user#index"
+  get "/users" => "user#show"
+  post "/users" => "user#create"
+  patch "/users" => "user#update"
+  delete "/users" => "user#destroy"
 
 #pin_drops
-  get "" => "pin_drop#ndex"
-  get "" => "pin_drop#show"
-  post "" => "pin_drop#create"
-  patch "" => "pin_drop#update"
-  delete "" => "pin_drop#destroy"
+  get "/pin_drops" => "pin_drop#ndex"
+  get "/pin_drops" => "pin_drop#show"
+  post "/pin_drops" => "pin_drop#create"
+  patch "/pin_drops" => "pin_drop#update"
+  delete "/pin_drops" => "pin_drop#destroy"
 
 #locations
-  get "" => "location#ndex"
-  get "" => "location#show"
-  post "" => "location#create"
-  patch "" => "location#update"
-  delete "" => "location#destroy"
+  get "/locations" => "location#ndex"
+  get "/locations" => "location#show"
+  post "/locations" => "location#create"
+  patch "/locations" => "location#update"
+  delete "/locations" => "location#destroy"
 
 #pin_drop_reactions
-get "" => "pin_drop_reaction#ndex"
-get "" => "pin_drop_reaction#show"
-post "" => "pin_drop_reaction#create"
-patch "" => "pin_drop_reaction#update"
-delete "" => "pin_drop_reaction#destroy"
+get "/pin_drop_reactions" => "pin_drop_reaction#ndex"
+get "/pin_drop_reactions" => "pin_drop_reaction#show"
+post "/pin_drop_reactions" => "pin_drop_reaction#create"
+patch "/pin_drop_reactions" => "pin_drop_reaction#update"
+delete "/pin_drop_reactions" => "pin_drop_reaction#destroy"
 
 end
